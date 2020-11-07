3d0632ae606fec8f9dcbcd3db68699f81dab02a2
Thiago Furtado <thiagojcf@live.com>
2020-11-07 20:00:05 +0000

    V1
    Line 10 changed and fixed.

--- src/barberhairdresseappointmentsystem/BarberHairdresseAppointmentSystem.java
+++ src/barberhairdresseappointmentsystem/BarberHairdresseAppointmentSystem.java
@@ -9,7 +9,7 @@ public class BarberHairdresseAppointmentSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TOODO code application logic here
         // LoginController login = new LoginController();
         new Controller();
     }
